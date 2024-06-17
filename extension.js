38284026971511f14515d2714fdf94c0a311d38f
Tr1 <dgminhtrii@gmail.com>
2024-06-17 18:14:56 +0700

    .
    .

---
+++ extension.js
@@ -0,0 +1,45 @@
+class FetchExtension {
+    getInfo() {
+      return {
+        id: 'fetchExtension',
+        name: 'Fetch Extension',
+        blocks: [
+          {
+            opcode: 'fetchData',
+            blockType: 'command',
+            text: 'FETCH url [URL] fetch what [QUERY] set fetch result to variable [VAR]',
+            arguments: {
+              URL: {
+                type: 'string',
+                defaultValue: 'https://api.example.com/your-url-here    '
+              },
+              QUERY: {
+                type: 'string',
+                defaultValue: 'data'
+              },
+              VAR: {
+                type: 'variable'
+              }
+            }
+          }
+        ]
+      };
+    }
+  
+    fetchData(args, util) {
+      const url = args.URL;
+      const query = args.QUERY;
+      return fetch(url)
+        .then(response => response.json())
+        .then(data => {
+          const result = data[query];
+          util.target.variables[args.VAR].value = result;
+        })
+        .catch(error => {
+          console.error("Error:", error);
+        });
+    }
+  }
+  
+  Scratch.extensions.register(new FetchExtension());
+  
